beba1eee0b2b02a23a2e6462d435d81a1b35aa94
Dagfinn1962 <130903243+Dagfinn1962@users.noreply.github.com>
2023-05-16 11:58:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def open_file(filepath):
         return infile.read()
 
 # Create an openaiapikey.txt file and save your api key.
-openai.api_key = ('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')
+openai.api_key = open_file('openaiapikey.txt')
 
 
 def bot(prompt, engine='text-davinci-002', temp=0.9, top_p=1.0, tokens=1000, freq_pen=0.0, pres_pen=0.5, stop=['<<END>>']):
